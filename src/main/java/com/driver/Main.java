8d82d259b8e16a0bbba94981afa7e4c7f9823a63
Shikhar Sharma <sharmashikhar9784@gmail.com>
2023-05-10 23:53:33 +0530

    Initial commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        //set name as rahul
+        //obj.name="Rahul";//this show error as name is private
+        //use set and get
+        obj.setName("Rahul");
+        System.out.print(obj.getName());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class RWOnly {
+    private String name;
+    void setName(String name)
+    {
+        this.name=name;
+    }
+    String getName()
+    {
+        return name;
+    }
     
 }
